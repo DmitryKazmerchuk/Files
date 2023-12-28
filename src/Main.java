51e8817f53346c6433a578ed4da4907d84076da7
Dmitry Kazmerchuk <dkazmerchuk@ittula.ru>
2023-12-28 11:30:34 +0300

    Change commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
 import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 public class Main {
 
@@ -6,46 +10,44 @@ public class Main {
 
         StringBuilder log = new StringBuilder();
         Logger logger = new Logger();
-        Logger newFiles = new Logger();
-        Logger newFolder = new Logger();
 
+        List<String> directories = List.of("c://Games/src", "c://Games/res", "c://Games/savegames", "c://Games/temp",
+                "c://Games/src/main", "c://Games/src/test", "c://Games/res/drawables", "c://Games/res/vectors", "c://Games/res/icons");
 
-        File src = new File("c://Games/src");
-        newFolder.createFolder(src, log);
+        List<String> files = List.of("c://Games/src/main/Main.java", "c://Games/src/main/Utils.java", "c://Games/temp/temp.txt");
+        log.append("------------------------------------------------------------ \n");
 
-        File res = new File("c://Games/res");
-        newFolder.createFolder(res, log);
+        for (String dir : directories) {
+            createFolder(new File(dir), log);
+        }
 
-        File savegames = new File("c://Games/savegames");
-        newFolder.createFolder(savegames, log);
+        for (String file : files) {
+            File newFile = new File(file);
+            createFiles(newFile, log);
+            logger.loggerWrite(newFile, log);
+        }
+    }
 
-        File temp = new File("c://Games/temp");
-        newFolder.createFolder(temp, log);
+    static SimpleDateFormat date = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 
-        File main = new File("c://Games/src/main");
-        newFolder.createFolder(main, log);
+    public static void createFiles(File nameFiles, StringBuilder log) {
+        try {
+            if (nameFiles.createNewFile()) {
+                log.append(date.format(new Date()) + " создан файл " + nameFiles.getName() + "\n");
+            } else {
+                log.append(date.format(new Date()) + " ошибка создания файла!!! " + nameFiles.getName() + "\n");
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
 
-        File test = new File("c://Games/src/test");
-        newFolder.createFolder(test, log);
-
-        File drawables = new File("c://Games/res/drawables");
-        newFolder.createFolder(drawables, log);
-
-        File vectors = new File("c://Games/res/vectors");
-        newFolder.createFolder(vectors, log);
-
-        File icons = new File("c://Games/res/icons");
-        newFolder.createFolder(icons, log);
-
-        File mainFiles = new File(main, "Main.java");
-        newFiles.createFiles(mainFiles, log);
-
-        File utilsFiles = new File(main, "Utils.java");
-        newFiles.createFiles(utilsFiles, log);
-
-        File tempFiles = new File(temp, "temp.txt");
-        newFiles.createFiles(tempFiles, log);
-        logger.loggerWrite(tempFiles,log);
+    public static void createFolder(File nameFolder, StringBuilder log) {
+        if (nameFolder.mkdir()) {
+            log.append(date.format(new Date()) + " создан каталог " + nameFolder.getName() + "\n");
+        } else {
+            log.append(date.format(new Date()) + " ошибка создания каталога!!! " + nameFolder.getName() + "\n");
+        }
     }
 }
 
